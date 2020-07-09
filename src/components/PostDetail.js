488e39335a76579ae9a0bee39c3b2ad2dcefe181
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-07-09 10:32:34 -0700

    add reactstrap

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,19 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 import DateTime from "./DateTime";
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 function PostDetail(props) {
   const { post, onClickingDelete } = props;
   return (
     <React.Fragment>
-      <h1>{post.title}</h1>
-      <h3>{post.body}</h3>
-      <DateTime time={post.time} />
-      <h3>{post.upvotes}</h3>
-      <h3>{post.downvotes}</h3>
-      <button onClick={props.onClickingEdit}>Update Post</button>
-      <button className="upVote" onClick={() => props.onClickingDelete(post.id)}>Delete</button>
+      <Card>
+        <CardBody>
+          <CardTitle><h1>{post.title}</h1></CardTitle>
+          <CardSubtitle><DateTime time={post.time} /> -- <h3>{post.upvotes}</h3></CardSubtitle>
+          <CardText><h3>{post.body}</h3></CardText>
 
+
+          {/* <h3>{post.downvotes}</h3> */}
+          <button onClick={props.onClickingEdit}>Update Post</button>
+          <button className="upVote" onClick={() => props.onClickingDelete(post.id)}>Delete</button>
+        </CardBody>
+      </Card>
     </React.Fragment>
   )
 }
@@ -23,6 +28,51 @@ PostDetail.propTypes = {
   onClickingDelete: PropTypes.func,
   onClickingEdit: PropTypes.func
 
+};
+
+Card.propTypes = {
+  // Pass in a Component to override default element
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  inverse: PropTypes.bool,
+  color: PropTypes.string,
+  body: PropTypes.bool,
+  className: PropTypes.string
+};
+
+CardBody.propTypes = {
+  // Pass in a Component to override default element
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  className: PropTypes.string
+};
+
+
+
+CardImg.propTypes = {
+  // Pass in a Component to override default element
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  className: PropTypes.string,
+  // Use top or bottom to position image via "card-img-top" or "card-img-bottom"
+  top: PropTypes.bool,
+  bottom: PropTypes.bool
+};
+
+
+CardSubtitle.propTypes = {
+  // Pass in a Component to override default element
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  className: PropTypes.string
+};
+
+CardText.propTypes = {
+  // Pass in a Component to override default element
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  className: PropTypes.string
+};
+
+CardTitle.propTypes = {
+  // Pass in a Component to override default element
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  className: PropTypes.string
 };
 
 export default PostDetail;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { createStore } from 'redux';
 import rootReducer from './reducers/index';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const store = createStore(rootReducer);
 
